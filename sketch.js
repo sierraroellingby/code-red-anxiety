4b7af55655f3bcf89f26daefba8d0efc27f961cc
sierraroellingby <55562338+sierraroellingby@users.noreply.github.com>
2019-12-10 22:12:20 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -60,15 +60,15 @@ function preload() {
   modules[4] = loadImage('pscircle3.jpg');
   modules[5] = loadImage('pscircle3.jpg');
   modules[6] = loadImage('pscircle4.jpg');
-  modules[7] = loadImage('psredcircle1.jpg');
+  modules[7] = loadImage('redexcl.jpg');
     modules[8] = loadImage('pscircle5.jpg');
       modules[9] = loadImage('pscircle5.jpg');
         modules[10] = loadImage('pscircle6.jpg');
-          modules[11] = loadImage('psredcircle1.jpg');
+          modules[11] = loadImage('redexcl.jpg');
             modules[12] = loadImage('pscircle7.jpg');
               modules[13] = loadImage('pscircle7.jpg');
                 modules[14] = loadImage('pscircle1.jpg');
-                  modules[15] = loadImage('psredcircle1.jpg');
+                  modules[15] = loadImage('redexcl.jpg');
 
 }
 
